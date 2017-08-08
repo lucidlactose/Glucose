ef40dd11c224a4f8658134881454e0b5d3a8422d
Jesus Villarruel <jesusvillarruel@student.hartnell.edu>
2017-08-08 10:46:24 -0700

    changed parts of the python file and added image tags in html

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class MainHandler(webapp2.RequestHandler):
 
     def post(self):
         base_url = "http://api.giphy.com/v1/gifs/search?"
-        search = self.request.get('query')
+        search = self.request.get('search')
         url_params = {'q': search, 'api_key': 'dc6zaTOxFJmzC', 'limit': 10}
         giphy_response = urllib2.urlopen(base_url + urllib.urlencode(url_params)).read()
         parsed_giphy_dictionary = json.loads(giphy_response)
@@ -25,7 +25,7 @@ class MainHandler(webapp2.RequestHandler):
         template_variables = {
             "url": gif_url
         }
-        results_template = jinja_environment.get_template("templates/results.html")
+        results_template = jinja_environment.get_template("templates/main.html")
 
         self.response.write(results_template.render(template_variables))
 
