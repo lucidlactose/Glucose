d22482618c746d53ccf26651f8d0aaa3b86b79c3
Jesus Villarruel <jesusvillarruel@student.hartnell.edu>
2017-08-08 15:14:32 -0700

    made a second handler

--- main.py
+++ main.py
@@ -10,25 +10,28 @@ jinja_environment = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template("templates/main.html")
+        template = jinja_environment.get_template("templates/start.html")
         self.response.write(template.render())
 
 
+
+
+class ResultHandler(webapp2.ResultHandler):
     def post(self):
         base_url = "http://api.giphy.com/v1/gifs/search?"
-        search = self.request.get('search')
-        url_params = {'q': search, 'api_key': 'dc6zaTOxFJmzC', 'limit': 10}
+        url_params = {'q': self.request.get('search'), 'api_key': 'dc6zaTOxFJmzC', 'limit': 10}
         giphy_response = urllib2.urlopen(base_url + urllib.urlencode(url_params)).read()
         parsed_giphy_dictionary = json.loads(giphy_response)
         gif_url = parsed_giphy_dictionary['data'][1]['images']['original']['url']
-
-        template_variables = {
             "url": gif_url
         }
         results_template = jinja_environment.get_template("templates/main.html")
 
         self.response.write(results_template.render(template_variables))
 
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler,
+     '/result', ResultHandler)
 ], debug=True)
+template_variables = {
