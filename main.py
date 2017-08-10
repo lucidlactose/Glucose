af0bcfbd150f32fa41e9918d33391846898eb2f8
Jesus Villarruel <jesusvillarruel@student.hartnell.edu>
2017-08-10 08:47:55 -0700

    ia:

--- main.py
+++ main.py
@@ -21,6 +21,12 @@ class MainHandler(webapp2.RequestHandler):
         url_params = {'q': search, 'api_key': 'dc6zaTOxFJmzC', 'limit': 10}
         giphy_response = urllib2.urlopen(base_url + urllib.urlencode(url_params)).read()
         parsed_giphy_dictionary = json.loads(giphy_response)
+# app_id  	3a2897b4
+        dict_base_url = "https://od-api.oxforddictionaries.com/api/v1/entries/en/"
+        dict_url_params = {'q': search, 'api_key': 'fa87cfff2bc23fc87f8679f9413c61b3'}
+        # dict_base_url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/"
+        # dict_url_params = {'': search, 'key': 'ed36c100-f856-46ac-8be5-0fd11b43872b'}
+        # dict_xml = urllib2.urlopen(dict_base_url + urllib.urlencode(dict_url_params)).read()
 
         used = []
         random_gif = random.randint(0,7)
@@ -39,6 +45,7 @@ class MainHandler(webapp2.RequestHandler):
         results_template = jinja_environment.get_template("templates/results.html")
 
         self.response.write(results_template.render(template_variables))
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
